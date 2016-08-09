6b7a1bdd45701ff1c64ead0fd6c2caef71828ea6
RexCorpDev <rexcorpfx@gmail.com>
2016-08-08 19:06:56 -0700

    send grid added

---
+++ server/db/models/sendGrid.js
@@ -0,0 +1,31 @@
+const Mail = {
+  verify(clientName, cb) {
+    const helper = require('sendgrid').mail;
+    const fromEmail = new helper.Email('registration@yelpCatalogue.com');
+    const toEmail = new helper.Email(`${clientName.Email}`);
+    const subject = 'Itinerary';
+    const content = new helper.Content('text/html',
+    `<html>
+    <h1>Hi, ${clientName.Firstname}</h1>
+    <br>
+    <h4>Thanks for using itinerEZ</h4>
+    <h1>Here is your itenerary</h1>
+    <p class="lead">blah blah blah</p>
+    <br>
+    <h2>Thanks!
+    <br>
+    Respectfully,
+    <br>
+    <i>Tobiah Rex</i></h2>
+    </html>`);
+    const mail = new helper.Mail(fromEmail, subject, toEmail, content);
+    const sg = require('sendgrid').SendGrid(process.env.SENDGRID_API_KEY);
+    const requestBody = mail.toJSON();
+    const request = sg.emptyRequest();
+    request.method = 'POST';
+    request.path = '/v3/mail/send';
+    request.body = requestBody;
+    sg.API(request, cb);
+  },
+};
+module.exports = Mail;
