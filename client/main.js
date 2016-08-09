08fa9f9e17922111e7b70a67040c4afd51afb0bb
RexCorpDev <rexcorpfx@gmail.com>
2016-08-08 18:13:28 -0700

    removed bad comp refs

--- client/main.js
+++ client/main.js
@@ -3,14 +3,10 @@ import { render } from 'react-dom'
 import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
 
 import App from './components/App.js'
-import Dashboard from './components/Dashboard/Dashboard.js'
-import About from './components/About/About.js'
 
 render(
     <Router history={browserHistory}>
-      <Route path="/" component={App}>
-        <IndexRoute component={Dashboard} />
-        <Route path="about" component={About} />
+      <Route path="/" component={App}>        
       </Route>
     </Router>,
   document.getElementById('js-main')
