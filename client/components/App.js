8d5d3fda66cc2c0888afc927bf42afc2ebf137dc
RexCorpDev <rexcorpfx@gmail.com>
2016-08-08 18:11:30 -0700

    removed bad component refs

--- client/components/App.js
+++ client/components/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react'
-import NavBar from './Navbar/Navbar.js'
-import Footer from './Footer/Footer.js'
 export default class App extends Component {
   constructor(props){
     super(props);
@@ -10,12 +8,10 @@ export default class App extends Component {
     return (
       <div id="main-container">
         <div className="text-center">
-          <NavBar />
           <div className="container">
             {this.props.children}
           </div>
         </div>
-        <Footer />
       </div>
     )
   }
