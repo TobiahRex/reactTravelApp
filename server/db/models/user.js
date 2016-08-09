797b3e2233b9f6e4c519e820f9488e8b33c26167
RexCorpDev <rexcorpfx@gmail.com>
2016-08-08 19:50:33 -0700

    TOBY: built switch block for update user method.

---
+++ server/db/models/user.js
@@ -0,0 +1,27 @@
+
+let user = {
+  who: {},
+  when: {},
+  where: {},
+  what: {},
+}
+
+const User = {
+  getUser(){
+    return user;
+  },
+  updateUser(newInfo, cb){
+    if (!newInfo.type) return cb({ Error: 'Did not provide update type for user.' });
+    let user = this.getUser();
+
+    switch(newInfo.type) {
+      case 'WHO_UPDATE': user.who = newInfo.who; break;
+      case 'WHEN_UPDATE': user.when = newInfo.when; break;
+      case 'WHERE_UPDATE': user.where = newInfo.where; break;
+      case 'WHAT_UPDATE': user.what = newInfo.what; break;
+      default:
+    };
+
+    return user;
+  }
+}
