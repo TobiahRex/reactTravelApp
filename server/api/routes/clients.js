1a5b632e038f7696209645d9a5948ee313dc3953
RexCorpDev <rexcorpfx@gmail.com>
2016-08-10 12:56:36 -0700

    built api for mongo db

--- server/api/routes/clients.js
+++ server/api/routes/clients.js
@@ -3,9 +3,13 @@ const router = new express.Router();
 const Client = require('../../db/models/client.js');
 
 router.route('/')
-.get((req, res) => Client.(res.handle))
-.put((req, res) => Client.updateUserInfo(req.body, res.handle))
-.delete((req, res) => Client.resetUser(res.handle));
+.get((req, res) => Client.find({}, res.handle))
+.delete((req, res) => Client.remove({}, res.handle));
+
+router.route('/:id')
+.get((req, res) => Client.findById(req.params.id, res.handle))
+.put((req, res) => Client.findByIdAndUpdate(req.params.id, req.body, 'new', res.handle))
+.delete((req, res) => Client.findByIdAndRemove(req.params.id, req.body));
 
 router.post('/email', (req, res) => Client.sendEmail(req.body, res.handle));
 
