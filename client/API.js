82785fb792078dd1f10143cefc49901d4515066b
RexCorpDev <rexcorpfx@gmail.com>
2016-08-11 14:39:53 -0700

    updated API with getRestaurants and getActivities

--- client/API.js
+++ client/API.js
@@ -46,47 +46,15 @@ const API = {
     .done()
     .fail(err => ServerActions.receivedError(err));
   },
-
   getRestaurants(id, city) {
-    get('/api/yelp', { city, id })
+    get(`/api/yelp/restaurants/${id}`, city)
     .done(res => ServerActions.receivedClientData(res))
     .fail(err => ServerActions.receivedError(err));
   },
-
-  // getLunch(id, city) {
-  //   get(`/api/yelp/lunch/${city}`)
-  //   .done(response => response.json())
-  //   .done(data => {
-  //     RestaurantActions.getLunch(data.businesses);
-  //     API.addClientData({
-  //       lunch: data.businesses
-  //     }, `${id}`)
-  //   })
-  //   .fail(err => console.log('err:', err))
-  // },
-  //
-  // getDinner(id, city) {
-  //   get(`/api/yelp/dinner/${city}`)
-  //   .done(response => response.json())
-  //   .done(data => {
-  //     RestaurantActions.getDinner(data.businesses);
-  //     API.addClientData({
-  //       dinner: data.businesses
-  //     }, `${id}`)
-  //   })
-  //   .fail(err => console.log('err:', err))
-  // },
-
   getActivities(id, city) {
-    get(`/api/yelp/activities/${city}`)
-    .done(response => response.json())
-    .done(data => {
-      ActivityActions.getActivities(data.businesses)
-      API.addClientData({
-        activities: data.businesses
-      }, `${id}`)
-    })
-    .fail(err => console.log('err:', err))
+    get(`/api/yelp/activities/${id}`, city)
+    .done(res => ServerActions.receivedClientData(res))
+    .fail(err => ServerActions.receivedError(err));
   },
 
 }
