a58a9adee00a9dc6465dcef545a12b996d6951d7
RexCorpDev <rexcorpfx@gmail.com>
2016-08-15 12:08:29 -0700

    changed when title and margin-bottom formatting

--- client/components/When.js
+++ client/components/When.js
@@ -43,29 +43,30 @@ export default class When extends Component {
 
     return(
       <div className="slide">
-        <Row>
-          <Col md="10">
-
-            <h1>When?</h1>
+        <div className="when-title">
+          <h5 className='when-title mui--text-display2'>WHEN are you going?</h5>
+        </div>
+        <Row id='when-row'>
+          <Col md="7" md-offset='3'>
 
             <DatePickerRange
-            firstOfWeek={1}
-            numberOfCalendars={1}
-            selectionType='range'
-            minimumDate={new Date()}
-            stateDefinitions={stateDefinitions}
-            defaultState="available"
-            showLegend={false}
-            value={this.state.value}
-            onSelect={this.handleSelect}/>
+              firstOfWeek={1}
+              numberOfCalendars={1}
+              selectionType='range'
+              minimumDate={new Date()}
+              stateDefinitions={stateDefinitions}
+              defaultState="available"
+              showLegend={false}
+              value={this.state.value}
+              onSelect={this.handleSelect}/>
 
           </Col>
           <Col md="2">
             <div className="intro-arrow mui--text-right">
               <a href="#questionnaire/3" onClick={this.nextPage}>
-              <Button variant="fab" color="primary">
-                <i className="fa fa-arrow-right"></i>
-              </Button>
+                <Button variant="fab" color="primary">
+                  <i className="fa fa-arrow-right"></i>
+                </Button>
               </a>
             </div>
           </Col>
