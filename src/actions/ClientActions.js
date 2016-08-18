a76103ad3eb0a1461de5c8d2ed6afaa50b757265
RexCorpDev <rexcorpfx@gmail.com>
2016-08-18 13:41:18 -0700

    added getClientData for specific client by id

--- src/actions/ClientActions.js
+++ src/actions/ClientActions.js
@@ -37,20 +37,30 @@ export function createClient() {
 export function addClientData(newData, clientId) {
   return (dispatch) => {
     const options = {
-      newData,
-      clientId,
-      method: 'PUT'
+      method: 'PUT',
+      body: {
+        newData,
+        clientId,
+      },
     };
     fetch(`/api/clients/${clientId}`, options)
     .then(res => res.json())
-    .then(parsedJson => dispatch(API.receivedClientData(res[0])))
-    .catch(err => dispatch(receivedError(err)));
-  }
-  return (dispatch) => dispatch(addClientData(newData, clientId));
+    .then(parsedJson => dispatch(API.receivedClientData(parsedJson[0])))
+    .catch(err => dispatch(API.receivedError(err)));
+  };
 }
 
 export function getClientData(clientId) {
-  return (dispatch) => dispatch(getClientData(clientId));
+  return (dispatch) => {
+    const options = {
+      method: 'GET',
+      body: clientId,
+    };
+    fetch(`/api/clients/${clientId}`)
+    .then(res => res.json())
+    .then(parsedJson => API.receivedClientData(parsedJson))
+    .catch(err => API.receivedError(err));
+  };
 }
 
 export function deleteClient(clientId) {
@@ -64,61 +74,3 @@ export function getItinerary(clientId, city) {
 export function submitEmail(clientId, email) {
   return (dispatch) => dispatch(submitEmail(clientId, email));
 }
-
-const API = {
-  getAllClientData() {
-    get('/api/clients/')
-    .done(res => ServerActions.receivedClientData(res))
-    .fail(err => ServerActions.receivedError(err));
-  },
-  deleteAllClients() {
-    ajax({
-      url: '/api/clients/',
-      method: 'DELETE',
-    })
-    .done(res => ServerActions.receivedClientData(res))
-    .fail(err => ServerActions.receivedError(err));
-  },
-  createClient() {
-    post('/api/clients/')
-    .done(res => ServerActions.receivedNewClient(res))
-    .fail(err => ServerActions.receivedError(err));
-  },
-  getClientData(clientId) {
-    get(`/api/clients/${clientId}`)
-    .done(res => { ServerActions.receivedClientData(res) })
-    .fail(err => ServerActions.receivedError(err));
-  },
-  addClientData(newClientData, clientId) {
-    ajax({
-      url: `/api/clients/${clientId}`,
-      method: 'PUT',
-      data: newClientData,
-    })
-    .done(res => ServerActions.receivedClientData(res[0]))
-    .fail(err => ServerActions.receivedError(err));
-  },
-  deleteClient(clientId) {
-    ajax({
-      url: `/api/clients/${clientId}`,
-      method: 'DELETE',
-    })
-    .done()
-    .fail(err => ServerActions.receivedError(err));
-  },
-  getItinerary(clientId, city) {
-    post(`/api/yelp/itinerary/${clientId}`, {
-      location: city,
-    })
-    .done(data => {
-      console.log('ITINERARY RESPONSE:', data);
-      ServerActions.receivedClientData(data);
-    })
-    .fail(err => ServerActions.receivedError(err));
-  },
-  submitEmail(clientId, email) {
-    post(`/api/clients/email/${clientId}`, email)
-    .done(res => ServerActions.sentEmailResponse(res))
-    .fail(err => ServerActions.receivedError(err));
-  },
-};
