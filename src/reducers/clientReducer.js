234c72747a8736adb4243e297a301ed782e42334
RexCorpDev <rexcorpfx@gmail.com>
2016-08-18 14:45:53 -0700

    started configuring initialState.js

---
+++ src/reducers/clientReducer.js
@@ -0,0 +1,10 @@
+import * as types from '../actions/actionTypes';
+
+function clientReducer(state = initialState.client, action) {
+  switch(action.type) {
+
+    case types.RECEIVE_ACTIVITIES:
+      
+
+  }
+}
