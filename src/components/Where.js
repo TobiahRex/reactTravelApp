bfa3fcde45d7a3f28545a996bd5fdf13d6991984
RexCorpDev <rexcorpfx@gmail.com>
2016-08-19 10:38:30 -0700

    Where comp. refactored with Redux.

--- src/components/Where.js
+++ src/components/Where.js
@@ -3,10 +3,9 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { browserHistory } from 'react-router';
 import ClientActions from '../actions/ClientActions';
-import ClientStore from '../stores/ClientStore';
 import { Panel, Container, Row, Col, Form, Input, Button } from 'muicss/react';
 
-export default class Where extends Component {
+class Where extends Component {
   constructor(props) {
     super(props);
 
@@ -15,8 +14,8 @@ export default class Where extends Component {
   }
 
   showItinerary() {
-    let client = ClientStore.getClient();
-    if(this.state.city === '') {
+    const client = this.props.client;
+    if (this.state.city === '') {
       swal({
         title: 'Whoa there!',
         text: 'If you want an itinerary, you\'re going to need to give us a destination.',
@@ -25,30 +24,30 @@ export default class Where extends Component {
         confirmButtonColor: '#f7b8b8',
       });
     } else {
-      ClientActions.addClientData({ where: { city: this.state.city }}, client._id);
-      ClientActions.getItinerary(client._id, this.state.city);
+      this.props.actions.addClientData({ where: { city: this.state.city } }, client._id);
+      this.props.actions.getItinerary(client._id, this.state.city);
     }
   }
 
   render() {
-    return(
+    return (
       <div className="slide">
         <div>
-          <h5 className='where-title mui--text-display2'>WHERE are you going?</h5>
+          <h5 className="where-title mui--text-display2">WHERE are you going?</h5>
         </div>
-        <Row id='where-row'>
+        <Row id="where-row">
 
-          <Col md='10'>
-            <Col md='1' md-offset='3'>
-              <i className="fa fa-map-marker" aria-hidden="true"></i>
+          <Col md="10">
+            <Col md="1" md-offset="3">
+              <i className="fa fa-map-marker" aria-hidden="true" />
             </Col>
-            <Col md='6'>
+            <Col md="6">
               <Input
-                id='where-input'
-                hint='San Francisco'
-                className='form-control'
+                id="where-input"
+                hint="San Francisco"
+                className="form-control"
                 type="text" placeholder="City"
-                onChange={ e => this.setState({ city: e.target.value })}/>
+                onChange={e => this.setState({ city: e.target.value })} />
             </Col>
           </Col>
 
@@ -56,8 +55,8 @@ export default class Where extends Component {
           <Col md="2">
             <div className="mui--text-right">
               <a href="#questionnaire/4" onClick={this.showItinerary}>
-                <Button  variant="fab" color="primary">
-                  <i className="fa fa-arrow-right"></i>
+                <Button variant="fab" color="primary">
+                  <i className="fa fa-arrow-right" />
                 </Button>
               </a>
             </div>
@@ -66,7 +65,7 @@ export default class Where extends Component {
         </Row>
 
       </div>
-    )
+    );
   }
 }
 
@@ -82,3 +81,5 @@ const mapStateToProps = (state, ownProps) => ({
 const mapDispatchToProps = (dispatch) => ({
   actions: bindActionCreators(ClientActions, dispatch),
 });
+
+export default connect(mapStateToProps, mapDispatchToProps)(Where);
