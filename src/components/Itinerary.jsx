ba27e30d56cc4ba36975b3b71802433c1c67b22a
RexCorpDev <rexcorpfx@gmail.com>
2016-08-19 11:05:22 -0700

    refactored Itineary comp. for Redux.

---
+++ src/components/Itinerary.jsx
@@ -0,0 +1,82 @@
+import React, { PropTypes, Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Button, Panel, Container, Row, Col } from 'muicss/react';
+import Day from './Day';
+import ClientActions from '../actions/ClientActions';
+
+
+class Itinerary extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+    };
+
+    this.renderCards = this.renderCards.bind(this);
+    this.submitEmail = this.submitEmail.bind(this);
+  }
+
+  submitEmail() {
+    const clientId = this.props.client._id;
+    const email = this.state.email;
+
+    ClientActions.sendEmail(clientId, email);
+  }
+
+  renderCards(day, index) {
+    return (
+      <Day key={index} day={day} client={this.props.client} num={index + 1} />
+    );
+  }
+
+  render() {
+    const { client } = this.props;
+    return (
+      <div className="slide">
+        <Row id="itinerary-row">
+          <Col md="12">
+            <div className="itinerary">
+              <If condition={client}>
+                <p className="city-in-itinerary">{client.where.city}</p>
+              </If>
+              Enter your email so we can send you this itinerary!
+              <br />
+              <input
+                type="email"
+                placeholder="Email"
+                value={this.state.email}
+                onChange={e => this.setState({ email: e.target.value })} />
+              <Button
+                color="primary"
+                id="button"
+                className="form-control"
+                size="large"
+                onClick={this.submitEmail}>Send</Button>
+              <br />
+              <If condition={this.props.client}>
+                {this.props.client.itinerary.map(this.renderCards)}
+              </If>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+Itinerary.propTypes = {
+  actions: PropTypes.func.isRequired,
+  client: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  client: state.client,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators(ClientActions, dispatch),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Itinerary);
--- src/components/When.js
+++ src/components/When.js
@@ -26,47 +26,37 @@ const stateDefinitions = {
 };
 
 
-export default class When extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-
-    this.handleSelect = this.handleSelect.bind(this);
-  }
-  render() {
-    return (
-      <div className="slide">
-        <div className="when-title">
-          <h5 className="when-title mui--text-display2">WHEN are you going?</h5>
-        </div>
-        <Row id="when-row">
-          <Col md="10">
-
-            <DatePickerRange
-              firstOfWeek={1}
-              numberOfCalendars={1}
-              selectionType="range"
-              minimumDate={new Date()}
-              stateDefinitions={stateDefinitions}
-              defaultState="available"
-              showLegend={false}
-              value={this.state.value} />
-
-          </Col>
-          <Col md="2">
-            <div className="when-arrow mui--text-right">
-              <a href="#questionnaire/3">
-                <Button variant="fab" color="primary">
-                  <i className="fa fa-arrow-right" />
-                </Button>
-              </a>
-            </div>
-          </Col>
-
-        </Row>
+export default function When() {
+  return (
+    <div className="slide">
+      <div className="when-title">
+        <h5 className="when-title mui--text-display2">WHEN are you going?</h5>
       </div>
-    );
-  }
+      <Row id="when-row">
+        <Col md="10">
+
+          <DatePickerRange
+            firstOfWeek={1}
+            numberOfCalendars={1}
+            selectionType="range"
+            minimumDate={new Date()}
+            stateDefinitions={stateDefinitions}
+            defaultState="available"
+            showLegend={false}
+            value={this.state.value} />
+
+        </Col>
+        <Col md="2">
+          <div className="when-arrow mui--text-right">
+            <a href="#questionnaire/3">
+              <Button variant="fab" color="primary">
+                <i className="fa fa-arrow-right" />
+              </Button>
+            </a>
+          </div>
+        </Col>
+
+      </Row>
+    </div>
+  );
 }
