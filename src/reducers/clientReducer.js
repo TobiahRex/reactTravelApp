00323f15dfc319c551b3fccc3c905975e56e40d0
RexCorpDev <rexcorpfx@gmail.com>
2016-08-19 17:59:53 -0700

    building with devtools extension

--- src/reducers/clientReducer.js
+++ src/reducers/clientReducer.js
@@ -3,14 +3,12 @@ import * as types from '../actions/actionTypes';
 import * as initialState from './initialState';
 
 function clientReducer(state = initialState.client, action) {
-  console.log('hi from reducer');
   switch (action.type) {
 
     case types.RECEIVED_DB_ERROR:
       return toastr.error('Could not GET client information. Sorry', 'ERROR');
 
     case types.RECEIVED_NEW_CLIENT:
-      console.log('action.dbClient: ', action.dbClient);
       return action.dbClient;
 
     case types.RECEIVED_UPDATED_CLIENT:
